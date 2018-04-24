48e874ee63bc874fa608606968d804ee948cf39d
ppolanco01 <ppolanco001@outlook.com>
2018-04-24 14:34:51 +0000

    Inital Django Setup

---
+++ pythonzoo/zoo/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    temporaryData = "Paul"
+    return render(
+        request,
+        "index.html",
+        context = { 'temporaryData' : temporaryData },
+    )
+
